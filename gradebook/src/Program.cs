4ea746316167ef108f623dd06dad224294a6f058
VitMasterB <jonnyfaust1988@gmail.com>
2020-01-15 22:52:16 +0100

    Was gelöscht

--- gradebook/src/Program.cs
+++ gradebook/src/Program.cs
@@ -7,7 +7,6 @@ namespace src
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-                        Console.WriteLine("Hello World!");
         }
     }
 }
